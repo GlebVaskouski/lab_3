c31e708f9116f6acc925e992181e4599e58743d9
GlebVaskouski <93393301+GlebVaskouski@users.noreply.github.com>
2021-10-29 19:12:30 +0300

    Update a1.h

--- solutions/a1.h
+++ solutions/a1.h
@@ -2,6 +2,9 @@
 #include <vector>
 
  bool AreMatricesIdentical(const std::vector<std::vector<int>>& first_vector, const std::vector<std::vector<int>>& second_vector) {
+  if ((first_vector.size() == 0) || (second_vector.size() == 0)) {
+   return fasle;
+  }
     for(int i = 0; i < first_vector.size(); i++)
     {
         for(int j = i + 1; j < first_vector.size(); j++) {
