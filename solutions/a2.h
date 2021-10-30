d4863c14127ef5ee1d316f9b7ceb67875fcdae28
GlebVaskouski <93393301+GlebVaskouski@users.noreply.github.com>
2021-10-30 19:30:15 +0300

    Update a2.h

--- solutions/a2.h
+++ solutions/a2.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <vector>
+
+int TheGreatestColumnSum(const std::vector<std::vector<int>>& first_vector) {
+    int max_sum;
+    for(int i = 0; i < first_vector.size(); i++)
+    {
+        int sum = 0;
+        for(int j = i + 1; j < first_vector.size(); j++) {
+            sum += first_vector[j][i];
+        }
+        if (sum > max_sum) {
+            max_sum = sum;
+        }
+    }
+    return max_sum;
+}
